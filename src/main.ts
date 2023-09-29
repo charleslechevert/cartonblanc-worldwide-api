56080c131f3326d0b3ea3fd0f217794becab1154
Charles LC <charles.lechevert@outlook.fr>
2023-09-29 19:14:06 +0200

    check

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
-import * as cookieParser from 'cookie-parser';
+import cookieParser from 'cookie-parser';
+
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import * as dotenv from 'dotenv';
